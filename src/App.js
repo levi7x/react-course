a8f7d05bf02a562d37edfa4c755f2d81aeb7bbe2
Marian Simo <sim0302@vsb.cz>
2022-12-05 22:52:07 +0100

    added basic render look "2:11"

--- src/App.js
+++ src/App.js
@@ -4,10 +4,12 @@ import { Route, Routes } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetupsPage from "./pages/NewMeetups";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from "./components/layout/MainNavigation";
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Routes>
         <Route path="/" element={<AllMeetupsPage />} />
         <Route path="/new-meetup" element={<NewMeetupsPage />} />
