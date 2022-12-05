6b0cf8ab695f1ba57a3e2385c8858d311a836f3f
Marian Simo <sim0302@vsb.cz>
2022-12-05 18:07:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Todo from "./components/Todo";
+
+function App() {
+  return <div>
+    <h1>My Todos</h1>
+    <Todo text="Learn React"/>
+    <Todo text="Hello"/>
+    <Todo text="Test"/>
+  </div>;
+}
+
+export default App;
