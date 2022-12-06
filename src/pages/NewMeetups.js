d1a5ced18ab871afaced6c8536fbe7916b6ad8af
Marian Simo <sim0302@vsb.cz>
2022-12-06 17:18:56 +0100

    first store on firebase db

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,5 +1,30 @@
+import { useNavigate } from "react-router-dom";
+
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupsPage() {
-    return <div>New Meetup Page</div>;
+const navigate = useNavigate();
+
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-course-d2566-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-type": "application/json",
+        },
+      }
+    ).then(() => {
+      navigate("/", {replace: true});
+    });
   }
-  
-  export default NewMeetupsPage;
+  return (
+    <section>
+      <h1>Add new meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
+}
+
+export default NewMeetupsPage;
